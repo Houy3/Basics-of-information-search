307ebca11795bb8cf61fb3d13651032418a0de26
fattakhovoe <fattakhovoe@FATTAHOV-OE>
2025-03-31 12:52:59 +0300

    work2 refactoring

--- Work2/process_text.py
+++ Work2/process_text.py
@@ -6,32 +6,27 @@ from common import lemmatize_tokens, tokenize, clean_html
 nltk.download('stopwords')
 russian_stopwords = set(stopwords.words('russian'))
 
+article_list_folder = "../Work1/result/article_list"
+OUTPUT_DIRECTORY = os.path.join(os.path.dirname(__file__), "result") # Выходная папка
+
+# читаем файл, чистим html теги и разбиваем на токены
 def process_files(folder_path):
-    seen_tokens = set()
-    all_tokens = []
-    for filename in sorted(os.listdir(folder_path), key=lambda x: int(x.split('.')[0])):
-        if filename.endswith('.txt'):
-            with open(os.path.join(folder_path, filename), 'r', encoding='utf-8') as f:
-                text = clean_html(f.read())
-                tokens = tokenize(text)
-                for token in tokens:
-                    token = token.lower()
-                    if token not in seen_tokens and token not in russian_stopwords:
-                        seen_tokens.add(token)
-                        all_tokens.append(token)
-    return all_tokens
+    tokens = set()
+    for filename in os.listdir(folder_path):
+        with open(os.path.join(folder_path, filename), 'r', encoding='utf-8') as f:
+            for token in tokenize(clean_html(f.read())):
+                token = token.lower()
+                if token not in russian_stopwords:
+                    tokens.add(token)
+    return list(tokens)
 
-def main():
-    folder_path = "../Work1/result/article_list"
-    tokens = process_files(folder_path)
-    lemmas = lemmatize_tokens(tokens)
+# main
+tokens = process_files(article_list_folder)
+lemmas = lemmatize_tokens(tokens)
 
-    with open("tokens.txt", "w", encoding="utf-8") as f:
-        f.write("\n".join(tokens))
+with open(os.path.join(OUTPUT_DIRECTORY, "tokens.txt"), "w", encoding="utf-8") as f:
+    f.write("\n".join(tokens))
 
-    with open("lemmas.txt", "w", encoding="utf-8") as f:
-        for lemma, words in lemmas.items():
-            f.write(f"{lemma} {' '.join(words)}\n")
-
-if __name__ == '__main__':
-    main()
+with open(os.path.join(OUTPUT_DIRECTORY, "lemmas.txt"), "w", encoding="utf-8") as f:
+    for lemma, words in lemmas.items():
+        f.write(f"{lemma} {' '.join(words)}\n")
