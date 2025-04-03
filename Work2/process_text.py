cbb9d2882572d2ecff257fe9b636b39305ba995d
fattakhovoe <fattakhovoe@FATTAHOV-OE>
2025-04-03 16:02:48 +0300

    fix by https://t.me/c/2140058324/665

--- Work2/process_text.py
+++ Work2/process_text.py
@@ -10,23 +10,27 @@ article_list_folder = "../Work1/result/article_list"
 OUTPUT_DIRECTORY = os.path.join(os.path.dirname(__file__), "result") # Выходная папка
 
 # читаем файл, чистим html теги и разбиваем на токены
-def process_files(folder_path):
+def process_files(file_name):
     tokens = set()
-    for filename in os.listdir(folder_path):
-        with open(os.path.join(folder_path, filename), 'r', encoding='utf-8') as f:
-            for token in tokenize(clean_html(f.read())):
-                token = token.lower()
-                if token not in russian_stopwords:
-                    tokens.add(token)
+    with open(file_name, 'r', encoding='utf-8') as f:
+        for token in tokenize(clean_html(f.read())):
+            token = token.lower()
+            if token not in russian_stopwords:
+                tokens.add(token)
     return list(tokens)
 
 # main
-tokens = process_files(article_list_folder)
-lemmas = lemmatize_tokens(tokens)
 
-with open(os.path.join(OUTPUT_DIRECTORY, "tokens.txt"), "w", encoding="utf-8") as f:
-    f.write("\n".join(tokens))
+for file_name in os.listdir(article_list_folder):
 
-with open(os.path.join(OUTPUT_DIRECTORY, "lemmas.txt"), "w", encoding="utf-8") as f:
-    for lemma, words in lemmas.items():
-        f.write(f"{lemma} {' '.join(words)}\n")
+    tokens = process_files(os.path.join(article_list_folder, file_name))
+    lemmas = lemmatize_tokens(tokens)
+
+    short_file_name = file_name.split('.')[0]
+
+    with open(os.path.join(OUTPUT_DIRECTORY, short_file_name + "-tokens.txt"), "w", encoding="utf-8") as f:
+        f.write("\n".join(tokens))
+
+    with open(os.path.join(OUTPUT_DIRECTORY, short_file_name + "-lemmas.txt"), "w", encoding="utf-8") as f:
+        for lemma, words in lemmas.items():
+            f.write(f"{lemma} {' '.join(words)}\n")
